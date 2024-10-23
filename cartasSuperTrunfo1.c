821d310e1ed246c4b69330906b83ec220a22eadb
ElenBiah <elenclemente09@gmail.com>
2024-10-23 13:15:49 +0000

    criei o printf

--- cartasSuperTrunfo1.c
+++ cartasSuperTrunfo1.c
@@ -2,9 +2,14 @@
 
 int main () {
 
+printf ("Jogo super trunfo basico\n");
+
 int PontosTuristicos;
 float população, area, PIB;
 char CodigoDaCidade, NomeDaCidade;
+
+printf ("Digite o codigo da cidade\n");
+scanf ("%s", &CodigoDaCidade);
 
 
 
