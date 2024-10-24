254397c29dfdc658fa4ab2290d986719d445a65b
ElenBiah <elenclemente09@gmail.com>
2024-10-24 15:27:31 +0000

    criei  o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,34 +1,47 @@
 #include <stdio.h>
 
-int main() {
+int main (){
 
-    printf("desafio super trunfo basico\n");
-    
-    int pontos turisticos;
-    float população, area, PIB;
-    char codigo da cidade, nome [50];
-    
-    printf ("digite o codigo da cidade:\n");
-    scanf("%s", &codigo da cidade);
+    printf("Jogo super trunfo basico\n");
 
-    printf("digite o nome da cidade:\n");
-    scanf("%s", &nome da cidade);
+    int população;
+    int pontos;
+    float area;
+    float pib;
+    char codigo;
+    char cidade;
+    char estado;
 
-    printf("digite a população:\n");
-    scanf("%f", &população);
+    printf("Digite o codigo da carta\n");
+    scanf("%s", &codigo);
 
-    printf("digite a area:\n");
-    scanf("%f", &área);
+    printf("Digite o nome do estado\n");
+    scanf("%s", &estado);
 
-    printf("digite o PIB:\n");
-    scanf("%f", &PIB);
+    printf("Digite o nome da cidade\n");
+    scanf("%s", &cidade);
 
-    printf("pontos turisticos:\n");
-    scanf("%d", &numero de pontos turisticos);
-   
-   printf("codigo da cidade: %s, nome da cidade: %s\n", codigo, nome);
-   printf("população: %f, area: %f\n",população, área);
-   printf("PIB: %f, pontos turisticos: %d\n", PIB, pontos turisticos);
+    printf("Digite a populção da cidade\n");
+    scanf("%d", &população);
 
-    return 0;
+    printf("Digite a quantidade de pontos turisticos\n");
+    scanf("%d", &pontos);
+
+    printf("Digite a area da cidade\n")
+    scanf("%f", &area);
+
+    printf("Digite o pib da cidade\n");
+    scanf("%f", &pib);
+
+    printf("codigo da carta: %s\n", codigo);
+    printf("nome do estado: %s\n", estado);
+    printf("nome da cidade: %s\n", cidade);
+    printf("população da cidade: %d\n", população);
+    printf("pontos turisticos: %d\n", pontos);
+    printf("area da cidade em km²: %f\n", area);
+    printf("pib da cidade: %f", pib);
+
+
+return 0;
+
 }
